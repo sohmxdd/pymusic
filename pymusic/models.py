4766aae24772ac4c3a1d7802cfd7523ab99ad3c4
Soham <soham.mishra206@gmail.com>
2026-09-14 00:05:18 +0530

    feat(models): define MidiNote and TrackEvents data models

---
+++ pymusic/models.py
@@ -0,0 +1,19 @@
+﻿from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class MidiNote:
+    start_tick: int
+    duration_tick: int
+    midi_note: int
+    velocity: int
+
+    @property
+    def end_tick(self) -> int:
+        return self.start_tick + self.duration_tick
+
+@dataclass
+class TrackEvents:
+    track_index: int
+    name: str
+    instrument_name: str
+    notes: list[MidiNote]
